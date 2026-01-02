66762d68124d4c2f95bbf19bb2c47ffef4be6741
Shivam16a <shivam123hjp@gmail.com>
2026-01-02 12:12:32 +0530

    models created

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,10 @@
+const User = require("../models/users-model.js");
+const bcrypt = require("bcryptjs");
+
+const register = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- backend/models/users-model.js
+++ backend/models/users-model.js
@@ -1,9 +1,35 @@
 const mongoose = require('mongoose');
 
 const usershema = new mongoose.Schema({
-    username:{},
-    email:{},
-    phone:{},
-    password:{},
-    prifipic:{},
-})
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:String,
+        required:true,
+    },
+    gendet:{
+        type:String,
+        required:true,
+        enum:["male","female"],
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6,
+    },
+    prifipic:{
+        type:String,
+        required:true,
+        default:"",
+    },
+});
+
+const User = new mongoose.model("User",usershema);
+module.exports = User;
