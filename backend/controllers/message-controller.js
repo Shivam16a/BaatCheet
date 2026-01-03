1e904b5c91b17288c82d564a3c99f98b44f81856
Shivam16a <shivam123hjp@gmail.com>
2026-01-03 15:55:29 +0530

    create send message features in message controller

--- backend/controllers/message-controller.js
+++ backend/controllers/message-controller.js
@@ -1,9 +1,37 @@
-
+const Conversation = require("../models/conversation-model.js");
+const Message = require("../models/message-model.js");
 const sendMessage = async (req, res) => {
     try {
-        
+        const { message } = req.body;
+        const { id: reciverId } = req.params;
+        const senderId = req.user._id;
+
+        let chats = await Conversation.findOne({
+            participants: { $all: [senderId, reciverId] }
+        })
+        if (!chats) {
+            chats = await Conversation.create({
+                participants: [reciverId, senderId],
+            })
+        }
+
+        const newMessage = new Message({
+            senderId,
+            reciverId,
+            message,
+            conversationId:chats._id
+        })
+        if(newMessage){
+            chats.message.push(newMessage._id)
+        }
+        //SOCKET.IO function
+
+        await Promise.all([chats.save(),newMessage.save()]);
+        return res.status(201).json(newMessage);
     } catch (error) {
-        
+        console.error(error);
+
+        return res.status(400).json("Error from message controller");
     }
 }
 
--- backend/routes/message-route.js
+++ backend/routes/message-route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const sendMessage = require("../controllers/message-controller.js");
+const isLogin = require("../middleware/isLogin.js");
 const router = express.Router();
 
-router.route("/send/:id",sendMessage);
+router.route("/send/:id",isLogin,sendMessage);
 
 module.exports = router;
