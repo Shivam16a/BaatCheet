571550e9330aee078cf29da96ba438156b692b9a
Shivam16a <shivam123hjp@gmail.com>
2026-01-11 23:17:41 +0530

    verifying user login data

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,29 @@ const connectDB = require("./config/db.js");
 const authRoute = require("./routes/auth-route.js");
 const messageRoute = require("./routes/message-route.js");
 const userRoute = require("./routes/users-route.js");
+const cors = require("cors");
+
+
+const corsOptions = {
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+};
 
 
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(cors(corsOptions))
 
-app.use("/api/auth",authRoute);
-app.use("/api/message",messageRoute);
-app.use("/api/user",userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/message", messageRoute);
+app.use("/api/user", userRoute);
 
 
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`server is running on port : ${PORT}`);
     connectDB();
 });
--- client/src/login/Login.jsx
+++ client/src/login/Login.jsx
@@ -1,14 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
+import { toast } from 'react-toastify';
+
+
+const url = "http://localhost:5500/api/auth/login";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
+    const handlechange = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        setUser({
+            ...user,
+            [name]: value
+        })
+    };
+    const handlesubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(user),
+                credentials: "include",
+            });
+            const res_data = await response.json();
+            console.log(response);
+            if (response.ok) {
+                setUser({
+                    email: "",
+                    password: "",
+                })
+                console.log(res_data);
+                toast.success(res_data.msg);
+                navigate("/");
+            }else{
+                toast.error(res_data.msg);
+            }
+            
+        } catch (error) {
+            console.log(error);
+            toast.error(res_data.msg);
+        }
+    }
     return (
         <div className="login-page d-flex justify-content-center align-items-center">
             <div className="login-card">
                 <h2 className="login-title">Login <span style={{ color: "#121111ff" }}>BaatCheet</span></h2>
 
-                <form className="login-form">
+                <form className="login-form" onSubmit={handlesubmit}>
                     <div className="form-group">
                         <label htmlFor="email">Email address</label>
                         <input
@@ -19,6 +64,8 @@ const Login = () => {
                             required={true}
                             autoComplete="off"
                             className="fs-5 fw-bolder"
+                            value={user.email}
+                            onChange={handlechange}
                         />
                     </div>
 
@@ -32,6 +79,8 @@ const Login = () => {
                             required={true}
                             autoComplete="off"
                             className="fs-5 fw-bolder"
+                            value={user.password}
+                            onChange={handlechange}
                         />
                     </div>
 
