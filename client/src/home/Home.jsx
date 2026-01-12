87dc29c0fdd505e211bb79b64f5e185b859b2a6b
Shivam16a <shivam123hjp@gmail.com>
2026-01-12 23:12:59 +0530

    creating home page starting phase

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { useAuth } from "../context/authContext";
+import "./Home.css"
 
 const Home = () => {
-  const { user, loading, isLoggedIn } = useAuth();
+    const { user, loading, isLoggedIn } = useAuth();
 
-  if (loading) {
-    return <h2>Loading...</h2>;
-  }
+    console.log(user);
+    if (loading) {
+        return <h2>Loading...</h2>;
+    }
 
-  if (!isLoggedIn || !user) {
-    return <h2>Please login</h2>;
-  }
+    if (!isLoggedIn || !user) {
+        return <h2>Please login</h2>;
+    }
 
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Hi {user.username}</p>
-    </div>
-  );
+    return (
+        <section className="container">
+            <div className="container glass row row-cols-2 ">
+                <div className="left">
+                    <h1>sidebar</h1>
+                    <p>Hi {user.username}</p>
+                </div>
+                <div className="right">
+                    <h1>message</h1>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Home;
